81543f1c49b3969c3aabe4fe8b8673959670a14f
gyubbb <152342801+KGB22@users.noreply.github.com>
2024-04-15 23:35:26 +0900

    240415

---
+++ src/Q11382.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q11382 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        long A = scn.nextLong();
+        long B = scn.nextLong();
+        long C = scn.nextLong();
+
+        System.out.println(A + B + C);
+    }
+
+}
